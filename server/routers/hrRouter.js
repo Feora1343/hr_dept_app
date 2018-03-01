405c7bc4dca978f1c0bd2d4b014ca02df37e7331
Elizabeth Wong <lizzwong@gmail.com>
2018-03-01 16:50:31 -0600

    router get is done

--- server/routers/hrRouter.js
+++ server/routers/hrRouter.js
@@ -16,3 +16,15 @@ const EmployeeSchema = new mongoose.Schema(
 );
 
 const Employee = mongoose.model('Employee', EmployeeSchema, 'employees');
+
+router.get('/', (request,response) => {
+    Employee.find({}, (error, foundEmployees)=>{
+        if (error){
+            console.log('Error on find Employees:', error);
+            response.sendStatus(500);
+        }
+        else {
+            response.send(foundEmployees);
+        }
+    })
+});
