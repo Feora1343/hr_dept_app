ae90fbefa7fcb27ba6d897d22d9b61a9809186e2
Elizabeth Wong <lizzwong@gmail.com>
2018-03-01 16:43:02 -0600

    full server

---
+++ server/routers/hrRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+//create schema
+
+const EmployeeSchema = new mongoose.Schema(
+    {
+        name: String,
+        employee_number: String,
+        salary: Number,
+        title: String,
+        hire_date: { type: Date, default: new Date()},
+
+    }
+)
