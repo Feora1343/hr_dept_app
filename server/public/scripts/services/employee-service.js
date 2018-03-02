9cf908f85dc466b3fcb89deef29e712197de11b9
Feora1343 <renee.vorbeck@gmail.com>
2018-03-01 22:58:08 -0600

    Added self.getEmployees to employee-services.js

--- server/public/scripts/services/employee-service.js
+++ server/public/scripts/services/employee-service.js
@@ -3,6 +3,16 @@ myApp.service('EmployeeService', ['$http', function($http) {
     const self = this;
     self.employee = {list: []};
 
+    self.getEmployee = function() {
+        $http.get('/employee')
+        .then(function(response){
+            self.employee.list = response.data;
+        })
+        .catch(function(response){
+            console.log('Error GETing employees!');
+        })
+    }
+
     self.addEmployee = function(employee) {
         console.log('Inside Add Employee Function!');
         
